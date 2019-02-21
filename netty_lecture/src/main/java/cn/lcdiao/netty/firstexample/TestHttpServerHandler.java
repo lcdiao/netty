608548c54818ddb06328f62192aad8dd0d6c9a87
雕 <35444916+lcdiao@users.noreply.github.com>
2019-02-21 17:53:27 +0800

    add

---
+++ netty_lecture/src/main/java/cn/lcdiao/netty/firstexample/TestHttpServerHandler.java
@@ -0,0 +1,24 @@
+package cn.lcdiao.netty.firstexample;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.*;
+import io.netty.util.CharsetUtil;
+
+public class TestHttpServerHandler extends SimpleChannelInboundHandler<HttpObject> {
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, HttpObject httpObject) throws Exception {
+        if(httpObject instanceof HttpRequest) {
+            ByteBuf content = Unpooled.copiedBuffer("Hello World", CharsetUtil.UTF_8);
+            FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_0, HttpResponseStatus.OK, content);
+
+            response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain");
+            response.headers().set(HttpHeaderNames.CONTENT_LENGTH, content.readableBytes());
+
+            channelHandlerContext.writeAndFlush(response);
+        }
+    }
+}
