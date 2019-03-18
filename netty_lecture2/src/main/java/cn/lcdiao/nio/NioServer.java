797020277d82f0b96de5cb7bba5f05aef9df414c
雕 <35444916+lcdiao@users.noreply.github.com>
2019-03-18 17:58:42 +0800

    add

---
+++ netty_lecture2/src/main/java/cn/lcdiao/nio/NioServer.java
@@ -0,0 +1,103 @@
+package cn.lcdiao.nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+
+/**
+ * Created by diao on 2019/3/18
+ */
+public class NioServer {
+
+    //保存所有客户端的连接信息
+    private static Map<String,SocketChannel> clientMap = new HashMap<>();
+
+    public static void main(String[] args) throws IOException {
+        //创建serverSocketChannel
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        //配置成非阻塞
+        serverSocketChannel.configureBlocking(false);
+        //从serverSocketChannel获取到服务器端的socket对象(ServerSocket)
+        ServerSocket serverSocket = serverSocketChannel.socket();
+        //绑定端口8899
+        serverSocket.bind(new InetSocketAddress(8899));
+
+        Selector selector = Selector.open();
+        //将serverSocketChannel注册到selector上，关注着连接事件
+        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+        while (true) {
+            try {
+                //阻塞的，会返回所关注的事件的数量！
+                selector.select();
+                //select()返回之后，获得所注册的所有的已经发生的事件的selectedKeys集合
+                Set<SelectionKey> selectionKeys = selector.selectedKeys();
+
+                selectionKeys.forEach(selectionKey -> {
+
+                    final SocketChannel client;
+
+                    try {
+                        //判断是否客户端连接事件
+                        if (selectionKey.isAcceptable()) {
+                            //强转为ServerSocketChannel（只注册ServerSocketChannel的连接事件到selector上）
+                            ServerSocketChannel server = (ServerSocketChannel)selectionKey.channel();
+                            //返回与之连接的SocketChannel对象（获得客户端的SocketChannel）
+                            client = server.accept();
+                            //配置成非阻塞
+                            client.configureBlocking(false);
+                            //将client注册到selector上，关注着读取事件
+                            client.register(selector,SelectionKey.OP_READ);
+
+                            String key = "[" + UUID.randomUUID().toString() +"]";
+                            //保存到map
+                            clientMap.put(key,client);
+
+                            //selectionKeys.remove(selectionKey);
+
+                          //判断是否读取事件
+                        } else if (selectionKey.isReadable()) {
+                            //获取client，需要强转为SocketChannel（只注册SocketChannel的读取事件到selector上）
+                            client = (SocketChannel)selectionKey.channel();
+                            //将数据保存到buffer
+                            ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+                            int count = client.read(readBuffer);
+                            //如果真的有数据读到buffer里
+                            if (count > 0) {
+                                //翻转
+                                readBuffer.flip();
+                                //以utf-8编码转换数据
+                                Charset charset = Charset.forName("utf-8");
+                                String receiveMessage = String.valueOf(charset.decode(readBuffer).array());
+
+                                System.out.println(client + ":" + receiveMessage);
+                            }
+
+                            //selectionKeys.remove(selectionKey);
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                    selectionKeys.clear();
+
+                });
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+}
--- netty_lecture2/src/main/java/cn/lcdiao/nio/NioTest12.java
+++ netty_lecture2/src/main/java/cn/lcdiao/nio/NioTest12.java
@@ -1,7 +1,16 @@
 package cn.lcdiao.nio;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.channels.spi.SelectorProvider;
+import java.util.Iterator;
+import java.util.Set;
 
 /**
  * Created by diao on 2019/3/15
@@ -18,7 +27,75 @@ public class NioTest12 {
 
         //获取Selector对象
 
-
         Selector selector = Selector.open();
+
+        for (int i = 0; i < ports.length; i++) {
+            ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+            //设置不阻塞
+            serverSocketChannel.configureBlocking(false);
+            ServerSocket serverSocket = serverSocketChannel.socket();
+            InetSocketAddress address = new InetSocketAddress(ports[i]);
+            serverSocket.bind(address);
+
+            //将当前的serverSocketChannel注册到selector，并且key是接收、连接这样的一个动作
+            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+            System.out.println("监听端口： " + ports[i]);
+        }
+
+        while (true) {
+            //返回的键的数量
+            int numbers = selector.select();
+            System.out.println("numbers: " + numbers);
+
+            Set<SelectionKey> selectionKeys = selector.selectedKeys();
+            System.out.println("selectionKeys: " + selectionKeys);
+
+            Iterator<SelectionKey> iter = selectionKeys.iterator();
+
+            while (iter.hasNext()) {
+                SelectionKey selectionKey = iter.next();
+
+                if (selectionKey.isAcceptable()) {
+                    ServerSocketChannel serverSocketChannel = (ServerSocketChannel) selectionKey.channel();
+                    SocketChannel socketChannel = serverSocketChannel.accept();
+                    socketChannel.configureBlocking(false);
+
+                    socketChannel.register(selector,SelectionKey.OP_READ);
+                    //连接后必须把key移除
+                    iter.remove();
+
+                    System.out.println("获得客户端连接： " + socketChannel);
+                    //是否可读
+                } else if (selectionKey.isReadable()) {
+                    SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
+
+                    int bytesRead = 0;
+                    while (true) {
+                        ByteBuffer buffer = ByteBuffer.allocate(512);
+
+                        buffer.clear();
+
+                        int read = socketChannel.read(buffer);
+
+                        if (read <= 0) {
+                            break;
+                        }
+                        buffer.flip();
+
+                        socketChannel.write(buffer);
+
+                        bytesRead += read;
+                    }
+                    System.out.println("读取： " + bytesRead + ",来自于： " + socketChannel);
+                    //用完后一定要remove
+                    iter.remove();
+                }
+            }
+
+        }
+
+
+
+
     }
 }
